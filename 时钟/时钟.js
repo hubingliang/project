7e536f65e999e85a8067900d1e4ded786ca90d7d
brown <1104524352@qq.com>
2017-04-14 09:30:46 +0800

    时钟

---
+++ 时钟/时钟.js
@@ -0,0 +1,32 @@
+function step() {
+    var now = new Date()
+    var hours = now.getHours()
+    var minutes = now.getMinutes()
+    var seconds = now.getSeconds()
+
+    if (hours > 12) {
+        hours = hours - 12
+    }
+    var secondDeg = seconds / 60 * 360
+    var minuteDeg = minutes / 60 * 360
+    var hourDeg = hours / 12 * 360
+
+    document.querySelector('.second-hand').style.transform = `rotate(${-90 + secondDeg}deg)`
+
+    document.querySelector('.minute-hand').style.transform = `rotate(${-90 + minuteDeg}deg)`
+
+    document.querySelector('.hour-hand').style.transform = `rotate(${-90 + hourDeg}deg)`
+}
+
+
+
+
+
+
+
+
+console.log(document.querySelector('.hour-hand'))
+
+setInterval(function () {
+    step()
+}, 1000)
