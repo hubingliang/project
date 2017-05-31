e4d167feae8db186e9912cf0b379a3d282adc069
brown <1104524352@qq.com>
2017-05-31 19:57:50 +0800

    增加登录服务器 注册和登录功能

--- Login/button.js
+++ Login/button.js
@@ -1,8 +1,8 @@
 signIn.onclick = function() {
-    let logindeg = -1
+    let logindeg = -2
     let signdeg = 90
     var int = setInterval(function() {
-
+        console.log('sd')
         login.style.transform = `rotateY(${logindeg}deg)`
         if (logindeg > -90) {
             logindeg = logindeg - 2
@@ -30,8 +30,47 @@ back.onclick = function() {
         if (signdeg >= 90) {
             login.style.transform = `rotateY(${logindeg}deg)`
             logindeg = logindeg + 2
-            console.log(signdeg)
             if (logindeg >= 0) {
+                clearInterval(int)
+            }
+        }
+    }, 10);
+}
+
+
+register.onclick = function() {
+    let logindeg = 2
+    let registerdeg = 90
+    var int = setInterval(function() {
+
+        login.style.transform = `rotateY(${logindeg}deg)`
+        if (logindeg < 90) {
+            logindeg = logindeg + 2
+        }
+        if (logindeg >= 90) {
+            Register.style.transform = `rotateY(${registerdeg}deg)`
+            registerdeg = registerdeg - 2
+            if (registerdeg <= 0) {
+                clearInterval(int)
+            }
+        }
+    }, 10);
+
+}
+
+back2.onclick = function() {
+    let logindeg = 90
+    let registerdeg = 0
+    var int = setInterval(function() {
+
+        Register.style.transform = `rotateY(${registerdeg}deg)`
+        if (registerdeg < 90) {
+            registerdeg = registerdeg + 2
+        }
+        if (registerdeg >= 90) {
+            login.style.transform = `rotateY(${logindeg}deg)`
+            logindeg = logindeg - 2
+            if (logindeg <= 0) {
                 clearInterval(int)
             }
         }
